e52418cd4c19bfe7d53ab08aa0b7c506314d4ad2
Liber <libermenna@gmail.com>
2020-06-04 14:43:54 -0300

    refactored code

--- choropleth.js
+++ choropleth.js
@@ -71,7 +71,7 @@ g.call(d3.axisBottom(x)
     .select('.domain')
     .remove()
 
-const ready = ( [us, education] ) => {
+const ready = ([ us, education ]) => {
 
     svg.append("g")
         .attr("class", "counties")
@@ -79,51 +79,33 @@ const ready = ( [us, education] ) => {
         .data(topojson.feature(us, us.objects.counties).features)
         .enter().append("path")
         .attr("class", "county")
-        .attr("data-fips", function(d) {
-        return d.id
-        })
-        .attr("data-education", function(d) {
-            var result = education.filter(obj => obj.fips == d.id)
-            if(result[0]){
-                return result[0].bachelorsOrHigher
-            }
-            //could not find a matching fips id in the data
-            console.log('could find data for: ', d.id);
+        .attr("data-fips", d => d.id )
+        .attr("data-education", d => {
+            let result = education.filter(obj => obj.fips == d.id)
+            if(result[0]) return result[0].bachelorsOrHigher
             return 0
         })
-        .attr("fill", function(d) { 
-            var result = education.filter(obj => obj.fips == d.id)
-            if(result[0]){
-                return color(result[0].bachelorsOrHigher)
-            }
+        .attr("fill", d => { 
+            let result = education.filter(obj => obj.fips == d.id)
+            if(result[0]) return color(result[0].bachelorsOrHigher)
             return color(0)
         })
         .attr("d", path)
-        .on("mouseover", function(d) {      
+        .on("mouseover", d => {      
             tooltip.style("opacity", .9); 
-            tooltip.html(function() {
-                var result = education.filter(function( obj ) {
-                return obj.fips == d.id;
-                });
-                if(result[0]){
-                return result[0]['area_name'] + ', ' + result[0]['state'] + ': ' + result[0].bachelorsOrHigher + '%'
-                }
+            tooltip.html(() => {
+                let result = education.filter(obj => obj.fips == d.id)
+                if(result[0]) return result[0]['area_name'] + ', ' + result[0]['state'] + ': ' + result[0].bachelorsOrHigher + '%'
                 return 0
             })
-        .attr("data-education", function() {
-            var result = education.filter(function( obj ) {
-                return obj.fips == d.id;
-            });
-            if(result[0]){
-                return result[0].bachelorsOrHigher
-            }
+        .attr("data-education", () => {
+            let result = education.filter(obj => obj.fips == d.id);
+            if(result[0]) return result[0].bachelorsOrHigher
             return 0
         })
         .style("left", (d3.event.pageX + 10) + "px") 
-        .style("top", (d3.event.pageY - 28) + "px"); }) 
-        .on("mouseout", function(d) { 
-        tooltip.style("opacity", 0); 
-        });
+        .style("top", (d3.event.pageY - 28) + "px") }) 
+        .on("mouseout", d => tooltip.style("opacity", 0))
 
     svg.append("path")
         .datum(topojson.mesh(us, us.objects.states, (a, b) => a !== b))
